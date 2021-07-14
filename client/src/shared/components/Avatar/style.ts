2752663415a42b37e94b5f994207a9d4d3848f86
alexcloudstar <alexcloudstar@gmail.com>
2021-07-14 18:20:47 +0300

    Provide UI Improvments 🎨

--- client/src/shared/components/Avatar/style.ts
+++ client/src/shared/components/Avatar/style.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import Box from '@material-ui/core/Box/Box';
 
 export const AvatarWrapper = styled(Box)<{ username: string }>`
-	margin: 0 5px;
+	margin: 6px 0 5px;
 	padding: 0 6px;
 	line-height: 1.5;
 	border-radius: 20px;
