512e3d03456930900ec8a0ffa09afeb2553f9b7c
alexcloudstar <alexcloudstar@gmail.com>
2021-07-20 13:15:27 +0300

    removed .github

--- client/src/shared/components/Body/Body.tsx
+++ client/src/shared/components/Body/Body.tsx
@@ -1,10 +1,14 @@
-import React, { Fragment, memo, useEffect, useMemo, useState } from 'react';
+import React, { memo, useEffect, useState } from 'react';
 import { FC } from 'react';
 import { BodyWrapper } from './style';
 import { BodyProps } from './types';
 import { v4 as uuidv4 } from 'uuid';
 import { io } from 'socket.io-client';
-import { messagesStateTypeWithMessageStateType, Typing } from 'src/shared';
+import {
+	messagesStateTypeWithMessageStateType,
+	messageStateType,
+	Typing
+} from 'src/shared';
 const Message = React.lazy(() => import('../Message/Message'));
 
 const socket = io('http://localhost:4000');
@@ -26,16 +30,14 @@ const Body: FC<BodyProps> = ({ profanityWords }): JSX.Element => {
 
 	return (
 		<BodyWrapper>
-			{messages?.map(
-				({ username, message }: { username: string; message: string }) => (
-					<Message
-						key={uuidv4()}
-						username={username}
-						message={message}
-						profanityWords={profanityWords}
-					/>
-				)
-			)}
+			{messages?.map(({ username, message }: messageStateType) => (
+				<Message
+					key={uuidv4()}
+					username={username}
+					message={message}
+					profanityWords={profanityWords}
+				/>
+			))}
 
 			<Typing messageState={messages} setMessages={setMessages} />
 		</BodyWrapper>
